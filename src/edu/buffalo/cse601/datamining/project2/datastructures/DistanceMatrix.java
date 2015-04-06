933640243838da72416ab4410a6449289dea8ec4
Vibhav Gupta <gupta.vibhav@outlook.com>
2015-04-06 04:16:53 -0400

    first commit

---
+++ src/edu/buffalo/cse601/datamining/project2/datastructures/DistanceMatrix.java
@@ -0,0 +1,35 @@
+package edu.buffalo.cse601.datamining.project2.datastructures;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.google.common.primitives.Doubles;
+
+import edu.buffalo.cse601.datamining.project2.distancemeasures.Euclidean;
+
+public class DistanceMatrix {
+
+	public double[][] generateDistanceMatrix(TreeMap<Integer, List<Double>> dataPoints) {
+		double[][] distanceMatrix = new double[dataPoints.size()][dataPoints.size()];
+		Euclidean dist = new Euclidean();
+		int i = 0, j = 0;
+		for (Map.Entry<Integer, List<Double>> entry : dataPoints.entrySet()) {
+			j = 0;
+			for (Map.Entry<Integer, List<Double>> entry2 : dataPoints.entrySet()) {
+				if (entry.getKey().equals(entry2.getKey())) {
+					//System.out.println(entry.getKey()+" "+entry2.getKey());
+					continue;
+				} else {
+					if (entry.getValue() != null && entry2.getValue() != null) {
+						distanceMatrix[i][j] = dist.getDistance(Doubles.toArray(entry.getValue()), Doubles.toArray(entry2.getValue()));
+					}
+				}
+				j++;
+			}
+			i++;
+		}
+		return distanceMatrix;
+	}
+
+}
---
+++ src/edu/buffalo/cse601/datamining/project2/utils/DistanceCalculator.java
@@ -0,0 +1,18 @@
+package edu.buffalo.cse601.datamining.project2.utils;
+
+public class DistanceCalculator {
+
+	private DistanceCalculator() {
+
+	}
+
+	public static double getEuclideanDistance(double x[], double y[], int vectorDimensions) {
+		double sum = 0;
+		double tmp = 0;
+		for (int i = 0; i < vectorDimensions; i++) {
+			tmp = Math.pow((x[i] - y[i]), 2);
+			sum = sum + tmp;
+		}
+		return (Math.sqrt(sum));
+	}
+}
